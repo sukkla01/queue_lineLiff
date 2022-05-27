ce70d13f2e15cf21b49b1dc65011bbbd8ec80b17
sukkla01 <sukkla01@gmail.com>
2022-05-27 16:22:07 +0700

    x

--- pages/queue-time.js
+++ pages/queue-time.js
@@ -6,36 +6,48 @@ import { useRouter } from 'next/router'
 
 
 const QueueTime = () => {
-  const router = useRouter()
-  const [selectId, setSelectId] = useState(0)
-  const { dep,date } = router.query
+    const router = useRouter()
+    const [selectId, setSelectId] = useState(0)
+    const { dep, date } = router.query
+    const [name, setName] = useState('')
+    const [userId, setUserId] = useState('')
+    const [picture, setPicture] = useState('')
+
 
     let data = ['17:00', '18:00']
 
 
-    const onSelect =(value)=>{
+    useEffect(() => {
+
+        // getData()
+        setName(localStorage.getItem('name'))
+        setUserId(localStorage.getItem('userId'))
+        setPicture(localStorage.getItem('picture'))
+    })
+
+    const onSelect = (value) => {
         setSelectId(value)
     }
 
     const onNext = (value) => {
 
         router.push({
-          pathname: '/queue-time',
-          query: { dep: dep,date :  dateShow},
+            pathname: '/queue-time',
+            query: { dep: dep, date: dateShow },
         })
-    
-    
-      }
 
-      const onBack = (value) => {
+
+    }
+
+    const onBack = (value) => {
 
         router.push({
-          pathname: '/queue-date',
-          query: { dep: dep},
+            pathname: '/queue-date',
+            query: { dep: dep },
         })
-    
-    
-      }
+
+
+    }
     return (
         <div>
             <NavHeader />
@@ -43,14 +55,32 @@ const QueueTime = () => {
             <div style={{ paddingTop: '20%' }}>
                 {/* <ProfilePage /> */}
                 <h6 style={{ color: 'black', paddingTop: 25, paddingLeft: 20, paddingRight: 15 }}>เลือกเวลาจองคิว</h6>
+                {/* Profile */}
+                <div style={{ backgroundColor: 'white', marginLeft: 15, marginRight: 10, height: 110, borderRadius: 15 }}>
+                    <div className='row' style={{ paddingTop: 15, paddingLeft: 10 }}>
+                        <div className='col-4'>
+                            <img src={picture} width={80} height={80} style={{ borderRadius: '50%' }} />
+
+                        </div>
+                        <div className='col-8'>
+                            <div className='row' style={{ fontSize: 15 }}>
+                                ชื่อ - สกุล : {name}
+                            </div>
+                            <div className='row' style={{ fontSize: 15, paddingTop: 20 }}>
+                                HN :
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Profile */}
 
 
                 <div className='row'>
                     {data.map((item, i) => {
                         console.log(item)
                         return <div className='col-4' key={i} >
-                            <div className='text-center' style={{ backgroundColor: 'white', marginLeft: 15, marginRight: 10, height: 30, width: 100, borderRadius: 15,marginTop:15 , borderColor: selectId == item ? '#00bfa5' : 'white', borderWidth: 1, borderStyle: 'solid'}}
-                            onClick ={()=>onSelect(item)}
+                            <div className='text-center' style={{ backgroundColor: 'white', marginLeft: 15, marginRight: 10, height: 30, width: 100, borderRadius: 15, marginTop: 15, borderColor: selectId == item ? '#00bfa5' : 'white', borderWidth: 1, borderStyle: 'solid' }}
+                                onClick={() => onSelect(item)}
                             >
                                 <p style={{ paddingTop: 5 }}>{item}</p>
                             </div>
@@ -61,20 +91,20 @@ const QueueTime = () => {
                 </div>
 
                 <div className='row' style={{ marginTop: 50, marginLeft: 10, marginRight: 10, marginBottom: 100 }} >
-          <div className='col-6'>
-            <Button type={"default"} shape="round" block size={'large'}  onClick={onBack} >
-              กลับ
-            </Button>
+                    <div className='col-6'>
+                        <Button type={"default"} shape="round" block size={'large'} onClick={onBack} >
+                            กลับ
+                        </Button>
 
-          </div>
-          <div className='col-6'>
-            <Button type={"primary"} shape="round" block size={'large'}  onClick={onNext} >
-              ถัดไป
-            </Button>
+                    </div>
+                    <div className='col-6'>
+                        <Button type={"primary"} shape="round" block size={'large'} onClick={onNext} >
+                            ถัดไป
+                        </Button>
 
-          </div>
+                    </div>
 
-        </div>
+                </div>
 
 
 
