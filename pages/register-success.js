6b0af377718ec9b723f814433c68ac0760b8d618
sukkla01 <sukkla01@gmail.com>
2022-05-31 11:56:48 +0700

    register success

--- pages/register-success.js
+++ pages/register-success.js
@@ -24,7 +24,7 @@ const RegisterSuccess = () => {
             console.log(res.data)
             setData(res.data)
         } catch (error) {
-            console.log(error)
+            console.log(error) 
         }
     }
 
