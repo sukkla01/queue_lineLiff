401ead8ec06ace14109666040bc0c7fa5964d97c
sukkla01 <sukkla01@gmail.com>
2022-05-31 11:51:52 +0700

    x

--- pages/register-success.js
+++ pages/register-success.js
@@ -3,12 +3,13 @@ import NavHeader from '../component/NavHeader';
 import axios from 'axios'
 import config from '../config'
 import { useRouter } from 'next/router'
+import QRCode from "react-qr-code";
 
 const BASE_URL = config.BASE_URL
 
 const RegisterSuccess = () => {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState([ ])
     const router = useRouter()
     const { userId } = router.query
 
@@ -37,7 +38,11 @@ const RegisterSuccess = () => {
                         <p></p>
                         <div className='text-center' style={{ marginTop: 0 }}>
                             <h4 style={{ color: '#3f51b5', paddingTop: 20 }}>ลงทะเบียนเรียบร้อยแล้ว {userId}</h4>
-                            <img src={data[0].picture} width={80} height={80} style={{ borderRadius: '50%' }} />
+                            <img src={data[0].picture} width={80} height={80} style={{ borderRadius: '50%', marginTop:20,marginBottom:20 }} />
+                            <p style={{ marginTop:10,fontSize:16  }}>{data[0].tname}</p>
+                            <p style={{ marginTop:-10,fontSize:30 }}>{data[0].hn}</p>
+
+                            <QRCode value={data[0].hn}  size ={120}/>
                         </div>
 
 
