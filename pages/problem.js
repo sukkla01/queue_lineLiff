240fc56da29a7cddd119341be40902694ead67e2
sukkla01 <sukkla01@gmail.com>
2022-07-07 10:29:50 +0700

    x

--- pages/problem.js
+++ pages/problem.js
@@ -4,6 +4,7 @@ import { Input, Button } from 'antd';
 import axios from 'axios'
 import config from '../config'
 import { useRouter } from 'next/router'
+import LoadingSkeleton from '../component/LoadingSkeleton';
 
 const BASE_URL = config.BASE_URL
 const token = config.token
@@ -16,9 +17,24 @@ const Problem = () => {
     const [alertM, setUAlertm] = useState("");
     const [isLoading, setIsLoading] = useState(true);
 
+
+    useEffect(() => {
+        localStorage.setItem('path', 'problem');
+        async function getData() {
+            const liff = (await import('@line/liff')).default
+            await liff.ready
+            const profile = await liff.getProfile()
+            setProfile(profile)
+            setFormData({...formData,user_id})
+            setIsLoading(false)
+
+        }
+        getData()
+    }, [])
+
     const onOk = async () => {
         try {
-            let res = await axios.post(`${BASE_URL}/add-problem`, formData,{ headers: { "token": token } })
+            let res = await axios.post(`${BASE_URL}/add-problem`, formData, { headers: { "token": token } })
             router.push({
                 pathname: '/problem-success'
             })
@@ -38,34 +54,39 @@ const Problem = () => {
             <NavHeader />
 
             <div style={{ paddingTop: '17%', textAlign: 'center' }}>
-                <div style={{ backgroundColor: 'white', marginLeft: 10, marginRight: 10, height: 330, borderRadius: 15 }}>
-                    <p style={{ fontSize: 18, paddingTop: 20 }}>แจ้งปัญหาใช้งาน</p>
+                {isLoading ? <div className='container' style={{ marginTop: 20 }}>
+                    <LoadingSkeleton />
+                    <LoadingSkeleton />
+                    <LoadingSkeleton />
+                </div> :
+                    <div style={{ backgroundColor: 'white', marginLeft: 10, marginRight: 10, height: 330, borderRadius: 15 }}>
+                        <p style={{ fontSize: 18, paddingTop: 20 }}>แจ้งปัญหาใช้งาน</p>
 
-                    <div className='container'>
-                        <TextArea onChange={e => {
-                            // setIsCode(false)
-                            setFormData({ ...formData, detail: e.target.value })
-                            setUAlertm('')
-                        }} value={formData.detail} rows={4} placeholder="กรอกข้อมูลปัญหาที่ต้องการแจ้ง" />
-                        <Input onChange={e => {
-                            // setIsCode(false)
-                            setFormData({ ...formData, tel: e.target.value })
-                            setUAlertm('')
-                        }}
-
-                            onKeyPress={(event) => {
-                                if (!/[0-9]/.test(event.key)) {
-                                    event.preventDefault();
-                                    console.log('dd')
-                                    setUAlertm('กรอกเฉพาะตัวเลข')
-                                }
+                        <div className='container'>
+                            <TextArea onChange={e => {
+                                // setIsCode(false)
+                                setFormData({ ...formData, detail: e.target.value })
+                                setUAlertm('')
+                            }} value={formData.detail} rows={4} placeholder="กรอกข้อมูลปัญหาที่ต้องการแจ้ง" />
+                            <Input onChange={e => {
+                                // setIsCode(false)
+                                setFormData({ ...formData, tel: e.target.value })
+                                setUAlertm('')
                             }}
-                            value={formData.tel} style={{ marginTop: 20 }} placeholder="เบอร์โทรติตด่อ" />
-                        <Button type="primary" block size={'large'} onClick={onOk} style={{ marginTop: 20 }} >
-                            บันทึก
-                        </Button>
-                    </div>
-                </div>
+
+                                onKeyPress={(event) => {
+                                    if (!/[0-9]/.test(event.key)) {
+                                        event.preventDefault();
+                                        console.log('dd')
+                                        setUAlertm('กรอกเฉพาะตัวเลข')
+                                    }
+                                }}
+                                value={formData.tel} style={{ marginTop: 20 }} placeholder="เบอร์โทรติตด่อ" />
+                            <Button type="primary" block size={'large'} onClick={onOk} style={{ marginTop: 20 }} >
+                                บันทึก
+                            </Button>
+                        </div>
+                    </div>}
             </div>
         </div>
     )
