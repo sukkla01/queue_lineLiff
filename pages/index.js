33fa69bfda6d390db016669c5ee4932236c294e7
sukkla01 <sukkla01@gmail.com>
2022-08-05 13:42:47 +0700

    x

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,8 @@ export default function Home() {
     const params = new URLSearchParams(queryString);
     let tkey = params.get('key');
     // alert(path)
-    tkey  =  tkey == null ? '': tkey
-    router.push(`/${tkey}`)
+
+    tkey == null ? '' : router.push(`/${tkey}`)
 
 
 
